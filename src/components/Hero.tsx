cdd77766dcfed81de5d4100ccf5cdab539ba603d
tanishapakki <tanishap22ecs@student.mes.ac.in>
2026-06-12 06:31:54 +0530

    image added

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,10 +2,19 @@ import { useEffect, useState } from "react";
 import './Hero.css'
 import TextPressure from './TextPressure.tsx';
 import { AnimatedSpan, Terminal, TypingAnimation } from "./ui/teminal.tsx";
+import { motion } from "framer-motion";
 
 
 export default function Hero() { 
+  const [showImage, setShowImage] = useState(false);
 
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowImage(true);
+    }, 4000); // adjust later
+
+    return () => clearTimeout(timer);
+  }, []);
 
   return (
     <section className="hero">
@@ -35,6 +44,13 @@ export default function Hero() {
 
       <div className="hero-right">
         <div className="terminal-wrapper">
+           <motion.div
+      animate={{
+        opacity: showImage ? 0 : 1,
+        scale: showImage ? 0.95 : 1,
+      }}
+      transition={{ duration: 0.8 }}
+    >
   <Terminal className="!bg-white !border-gray-200 !text-gray-800 shadow-lg">
     <TypingAnimation className="!text-gray-800">
       {"> initialize_portfolio.exe"}
@@ -50,6 +66,20 @@ export default function Hero() {
       [████████████████████] 100%
     </AnimatedSpan>
   </Terminal>
+  </motion.div>
+
+    <motion.img
+      src="../src/assets/tanisha.png"
+      alt="Tanisha"
+      className="hero-image"
+      initial={{ opacity: 0, scale: 0.8 }}
+      animate={{
+        opacity: showImage ? 1 : 0,
+        scale: showImage ? 1 : 0.8,
+      }}
+      transition={{ duration: 1 }}
+    />
+
 </div>
       </div>
 
